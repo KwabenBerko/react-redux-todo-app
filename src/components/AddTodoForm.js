59d17ca1032c10f5c8f8728f9b5bf3239a9acfb8
KwabenBerko <kwabenaberko95@gmail.com>
2018-08-25 23:55:02 +0000

    Minor Refactoring

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { addTodo } from "../actions/postActions";
+import { addTodo } from "../actions/todoActions";
 
 class AddTodoForm extends Component {
   constructor() {
